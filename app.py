07c17a6b0051204d21c151106d0899e5e20099c1
navs9 <98230653+navs9@users.noreply.github.com>
2022-05-27 10:28:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ ip = st.text_input('Enter your message')
 op = model.predict([ip])           
 if st.button('Predict'):   
   st.title(op[0]) 
+  
+  
+  
+  
+             
 
                                   
                
